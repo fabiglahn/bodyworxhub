390f1375d548ecf3c51f6e9f0eee622f28116ae9
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-24 14:25:46 +0100

    ✨ Add first MongoDB additions

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,29 @@
 import express from "express";
-import path from "path";
+// import path from "path";
+import { connectDatabase, getUserCollection } from "../src/utils/database";
+import dotenv from "dotenv";
+dotenv.config();
 
 const port = process.env.PORT || 3001;
 const app = express();
+
+if (!process.env.MONGODB_URI) {
+  throw new Error("No MongoDB URI available");
+}
+
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
+
+// Get all users
+app.get("/api/users", async (_request, response) => {
+  const userCollection = getUserCollection();
+  const cursor = userCollection.find();
+  const allUsers = await cursor.toArray();
+  response.send(allUsers);
+});
 
 app.get("/api/hello", (_request, response) => {
   response.json({ message: "Hello from server" });
@@ -12,10 +33,10 @@ app.get("/api/hello", (_request, response) => {
 app.use(express.static("dist"));
 
 // Handle client routing, return all requests to the app
-app.get("*", (_request, response) => {
+/* app.get("*", (_request, response) => {
   response.sendFile(path.join(__dirname, "../dist/index.html"));
-});
+}); */
 
-app.listen(port, () => {
+/* app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-});
+}); */
