2bd34385ef897a8415d2b6f1bd1f80790f95e32b
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-11-26 11:10:26 +0100

    🎨 Change CSS for login form

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -4,13 +4,16 @@ import styles from "./LoginForm.module.css";
 function LoginForm() {
   return (
     <form className={styles.form}>
-      <input type="text" placeholder="email" className={styles.email} />
-      <input
-        type="password"
-        placeholder="password"
-        className={styles.password}
-      />
+      <div className={styles.input}>
+        <input type="text" placeholder="email" className={styles.email} />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.password}
+        />
+      </div>
       <div className={styles.submit}>
+        <span className={styles.logintext}>login</span>
         <input type="submit" value="→" className={styles.login} />
       </div>
     </form>
