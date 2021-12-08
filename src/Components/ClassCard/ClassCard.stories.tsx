c1d31f8a99c77e7185068acea1ffed5a250db9e4
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-12-08 14:51:29 +0100

    ✨ Add class card component for profile page

---
+++ src/Components/ClassCard/ClassCard.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ClassCard from "./ClassCard";
+
+export default {
+  component: ClassCard,
+  title: "Components/ClassCard",
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export const Default = () => <ClassCard />;
