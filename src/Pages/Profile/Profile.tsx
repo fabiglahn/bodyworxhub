b637ce94954bd5a7a9551dff7ef7bd201cd32539
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-12-09 12:04:05 +0100

    ✨ Add all components to page

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ClassCard from "../../Components/ClassCard/ClassCard";
+import NavigationBar from "../../Components/NavigationBar/NavigationBar";
 import ProfilePicture from "../../Components/ProfilePicture/ProfilePicture";
 import styles from "./Profile.module.css";
 
@@ -8,7 +9,16 @@ function Profile(): JSX.Element {
     <div className={styles.container}>
       <h2 className={styles.headline}>Profile</h2>
       <ProfilePicture />
+      <h3 className={styles.name}>Anna Wohlrab</h3>
+      <div className={styles.stats}>
+        <span className={styles.stats_headline}>Check-Ins</span>
+        <span className={styles.stats_headline}>Booked</span>
+        <span className={styles.stats_numbers}>77</span>
+        <span className={styles.stats_numbers}>1</span>
+      </div>
+      <h4 className={styles.booked}>Booked classes</h4>
       <ClassCard />
+      <NavigationBar activeLink={"profile"} />
     </div>
   );
 }
